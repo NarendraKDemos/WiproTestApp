5b82e596a2b8a87f86be55d34a0b76a3490a24bf
NarendraKDemos <techguynarendra@outlook.com>
2019-01-26 21:07:03 +0530

    Added Feeds, Feed Model

---
+++ app/src/main/java/com/techguys/wiprotestapp/ui/models/Feed.kt
@@ -0,0 +1,10 @@
+package com.techguys.wiprotestapp.ui.models
+
+data class Feed(
+
+	val imageHref: String? = null,
+
+	val description: String? = null,
+
+	val title: String? = null
+)
---
+++ app/src/main/java/com/techguys/wiprotestapp/ui/models/Feeds.kt
@@ -0,0 +1,4 @@
+package com.techguys.wiprotestapp.ui.models
+
+data class Feeds(val title: String? = null,
+				 val rows: List<Feed?>? = null)
