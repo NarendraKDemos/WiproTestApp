7a981dec7b78426bae1ef8084d24ba911c1e4ba4
NarendraKDemos <techguynarendra@outlook.com>
2019-01-26 15:03:53 +0530

    Added abstract methods and di code in BaseActivity

---
+++ app/src/main/java/com/techguys/wiprotestapp/ui/base/BaseActivity.kt
@@ -0,0 +1,22 @@
+package com.techguys.wiprotestapp.ui.base
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.techguys.wiprotestapp.WiproTestApplication
+import com.techguys.wiprotestapp.di.components.AppComponent
+
+abstract class BaseActivity: AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        injectDependencies((application as WiproTestApplication).getAppComp())
+        setContentView(getActivityLayout())
+        init(savedInstanceState)
+    }
+
+    protected abstract fun injectDependencies(appComponent: AppComponent)
+
+    protected abstract fun getActivityLayout():Int
+
+    protected abstract fun init(savedInstanceState: Bundle?)
+}
