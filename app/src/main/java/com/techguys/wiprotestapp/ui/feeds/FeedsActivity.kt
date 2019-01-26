77b39ba4898e6521da8b9d39ef86c2b16305f392
NarendraKDemos <techguynarendra@outlook.com>
2019-01-26 12:21:58 +0530

    Renamed MainActivity to FeedsActivity and moved it to correct package

--- app/src/main/java/com/techguys/wiprotestapp/ui/feeds/FeedsActivity.kt
+++ app/src/main/java/com/techguys/wiprotestapp/ui/feeds/FeedsActivity.kt
@@ -1,12 +1,13 @@
-package com.techguys.wiprotestapp
+package com.techguys.wiprotestapp.ui.feeds
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.techguys.wiprotestapp.R
 
-class MainActivity : AppCompatActivity() {
+class FeedsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_feeds)
     }
 }
