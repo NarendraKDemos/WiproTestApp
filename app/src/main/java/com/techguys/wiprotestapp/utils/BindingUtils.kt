33e02a387edf9c17714bae2325b640ff688e3076
NarendraKDemos <techguynarendra@outlook.com>
2019-02-06 23:02:40 +0530

    Enabled caching in Glide

--- app/src/main/java/com/techguys/wiprotestapp/utils/BindingUtils.kt
+++ app/src/main/java/com/techguys/wiprotestapp/utils/BindingUtils.kt
@@ -3,11 +3,14 @@ package com.techguys.wiprotestapp.utils
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
 import com.techguys.wiprotestapp.R
 
 val requestOptions = RequestOptions().apply {
     placeholder(R.drawable.image_placeholder)
+    diskCacheStrategy(DiskCacheStrategy.ALL)
+    skipMemoryCache(false)
     error(R.drawable.image_placeholder)
 }
 
@@ -18,4 +21,3 @@ fun setImageUrl(view: ImageView, url: String?) {
         .load(url)
         .into(view)
 }
-
